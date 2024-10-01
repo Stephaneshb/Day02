b2f3102d3bcd795aa262fe418e57af2a442893dd
Stephaneshb <stephane.shalaby@epitech.digital>
2024-10-01 16:50:57 +0200

    Create ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,9 @@
+function getBasketContent() {
+  // Liste des fruits restants en stock
+  const fruits = [
+    'strawberry', 'strawberry', 'strawberry',
+    'apple', 'apple',
+    'lime', 'lime',
+    'peach',
+    'pear', 'pear'
+  ];
