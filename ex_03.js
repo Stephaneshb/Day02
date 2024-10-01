f99ccedb9c2ce98371aae8c2b1a4b4dc1e7c2ff8
Stephaneshb <stephane.shalaby@epitech.digital>
2024-10-01 11:58:32 +0200

    Create ex_03.js

---
+++ ex_03.js
@@ -0,0 +1,18 @@
+function changebackground() {
+
+  
+{let magicNumber = 1;
+  if (magicNumber === 1) 
+    paintYellowRectangle();}
+  
+  {let magicNumber = 2;
+  if (magicNumber === 2) 
+    paintGreenRectangle();}
+  
+  {var magicNumber = 3;
+  if (magicNumber === 3) 
+    paintRedRectangle();}
+  
+}
+
+changebackground();
